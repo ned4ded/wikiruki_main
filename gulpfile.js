a0b8c6cfbcbad8727519dac70e15d97d2558c272
Bazhenov Eugen <wtf.czabe@gmail.com>
2017-11-01 21:06:22 +0300

    Begin markup, full config project env

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,51 @@
 const gulp = require('gulp');
 // Include plugins
+
+// JS plugins
+const uglify = require('gulp-uglify');
 // const jshint = require('gulp-jshint');
-// const sass = require('gulp-sass');
+
+// CSS Plugins
+const sass = require('gulp-sass');
+const cleanCss = require('gulp-clean-css');
+const csscomb = require('gulp-csscomb');
+const postcss = require('gulp-postcss');
+// Postcss Plugins
+const autoprefixer = require('autoprefixer');
+const mqpacker = require('css-mqpacker');
+
+// Common Plugins
 // const concat = require('gulp-concat');
-// const uglify = require('gulp-uglify');
-// const rename = require('gulp-rename');
+const rename = require('gulp-rename');
+
+// Webserver plugins
 const browserSync = require('browser-sync').create();
+const mainBowerFiles = require('main-bower-files');
+// End of including plugins
+
+// Inititialization tasks
+gulp.task('main-bower-files', function() {
+  gulp.src(mainBowerFiles('**/*.js'))
+    .pipe(uglify())
+    .pipe(rename(function(path) {
+      path.extname = ".min.js"
+    }))
+    .pipe(gulp.dest('app/js'));
+
+  gulp.src(mainBowerFiles('**/*.css'))
+    .pipe(cleanCss({debug: true}, function(details) {
+      console.log(details.name + ': ' + details.stats.originalSize);
+      console.log(details.name + ': ' + details.stats.minifiedSize);
+    }))
+    .pipe(rename(function(path) {
+      path.extname = ".min.css"
+    }))
+    .pipe(gulp.dest('app/css'));
+    return;
+});
 
 // Simple server
-gulp.task('browser-sync', function() {
+gulp.task('webserver', function() {
   browserSync.init({
     server: './app',
     files: './app/index.html',
@@ -18,16 +55,32 @@ gulp.task('browser-sync', function() {
   });
 });
 
+// Files transfer tasks
 gulp.task('html', function() {
-  return gulp.src('./*.html')
-  .pipe(reload({stream:true}));
-})
+  return gulp.src('./*.html');
+});
 
-gulp.task('live', ['browser-sync'], function() {
-  gulp.watch("*.html", ['html']);
-})
+gulp.task('styles', function() {
+  const plugins = [
+    autoprefixer({browsers: ['last 4 version']}),
+    mqpacker()
+  ];
+  return gulp.src('./styles/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(rename('custom.css'))
+    .pipe(gulp.dest('app/css'));
+});
 
+gulp.task('live', ['webserver'], function() {
+  //Watcher for index
+  browserSync.reload('./app/*.html', ['html']);
 
+  //Wathcer for styles
+  browserSync.reload('./styles/*.scss', ['styles']);
+});
 
 //Default
-// gulp.task('default', ['webserver']);
+gulp.task('default', ['live']);
+
+//Task on first time start
+gulp.task('init', ['main-bower-files', 'styles'])
