c5066f1b65ee45cbd2d56fb42464f3c2849b0ed8
Bazhenov Eugen <wtf.czabe@gmail.com>
2017-11-07 21:11:22 +0300

    Add animation for slider's dots

--- js/carousel.js
+++ js/carousel.js
@@ -1,12 +1,15 @@
-console.log('hi carousel');
+const customCarousel = $('.bilboard__slider').slick({
+  arrows: false,
+  dots: true,
+  dotsClass: 'bilboard__slider-dots',
+  appendDots: $('.bilboard'),
+  pauseOnHover: false,
+  pauseOnDotsHover: false,
+  infinite: true,
+  autoplay: true,
+  autoplaySpeed: 4000
+});
+
 $('document').ready(function(){
-  $('.bilboard__slider').slick({
-    arrows: false,
-    dots: true,
-    dotsClass: 'bilboard__slider-dots',
-    appendDots: $('.bilboard'),
-    infinite: false,
-    autoplay: false,
-    autoplaySpeed: 3000
-  })
+  customCarousel.slick('slickPlay');
 });
